25c671728427c53073460e366be0606a8b029a78
Cristina <crispv9@hotmail.com>
2020-02-28 23:52:13 +0100

    upñoad

---
+++ sketch.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  $("#image2").mouseover(function(){
+  $("#drug").show();
+  });
+  $("#image2").mouseout(function(){
+  $("#drug").hide();
+  });
+
+  $("#image").mouseover(function(){
+  $("#forgot").show();
+  });
+  $("#image").mouseout(function(){
+  $("#forgot").hide();
+  });
+
+  $("#image3").mouseover(function(){
+  $("#see").show();
+  });
+  $("#image3").mouseout(function(){
+  $("#see").hide();
+  });
+
+    $("#image4").mouseover(function(){
+    $("#presis").show();
+    });
+    $("#image4").mouseout(function(){
+    $("#presis").hide();
+    });
+
+    $("#image5").mouseover(function(){
+    $("#eyes").show();
+    });
+    $("#image5").mouseout(function(){
+    $("#eyes").hide();
+    });
+
+    $("#image6").mouseover(function(){
+    $("#nations").show();
+    });
+    $("#image6").mouseout(function(){
+    $("#nations").hide();
+    });
+
+    $("#image7").mouseover(function(){
+    $("#neo").show();
+    });
+    $("#image7").mouseout(function(){
+    $("#neo").hide();
+    });
+
+    $("#image8").mouseover(function(){
+    $("#heres").show();
+    });
+    $("#image8").mouseout(function(){
+    $("#heres").hide();
+    });
+
+    $("#image9").mouseover(function(){
+    $("#coding").show();
+    });
+    $("#image9").mouseout(function(){
+    $("#coding").hide();
+    });
+});
